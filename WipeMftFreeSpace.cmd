c711ba54dddc02ffb19b425292a02cc25a4115b6
harryytm <harryytm@gmail.com>
2020-04-26 12:14:50 +0800

    Update WipeMftFreeSpace.cmd

--- WipeMftFreeSpace.cmd
+++ WipeMftFreeSpace.cmd
@@ -3,12 +3,7 @@ cls
 set input=100
 
 :start
-title Wipe MFT free space tool
-echo Current path: %~dp0
-echo.
-echo  - Enter number of MFT record to wipe or leave empty to use default
-echo  - Enter 0 to exit
-echo.
+call :msg_main
 set /p input=Input(%input%): 
 
 if "%input%" == "0" goto exit
@@ -28,5 +23,14 @@ cls
 echo Wiped %input% unused MFT record(s).
 echo.
 goto start
+
+:msg_main
+title Wipe MFT free space tool
+echo Current path: %~dp0
+echo.
+echo  - Enter number of MFT record to wipe or leave empty to use default
+echo  - Enter 0 to exit
+echo.
+goto :eof
 
 :end
